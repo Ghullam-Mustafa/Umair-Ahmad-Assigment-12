553b2b86a05d4ac6434cb831757a6832282da557
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-21 20:47:40 +0500

    complete html

---
+++ script.js
@@ -0,0 +1,6 @@
+let elements = document.getElementsByTagName('p')
+console.log(elements[2].innerHTML);
+for (let index = 0; index < elements.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
