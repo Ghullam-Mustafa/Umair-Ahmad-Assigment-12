a17ebfd3fc548e2cd043ed421fc322b321a8d5f2
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-26 20:15:08 +0500

    r

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-// var elements = document.getElementsByClassName("abc");
-// console.log(elements[0]);
+// var elements = document.getElementsByTagName("p")
+// elements[2].className += " red"
+function name() {
+    
+
+    var now = new Date();
+    var today = now.getDay();
+
+    switch (today) {
+        case 0:
+            alert("It's sunday")
+            break;
+        case 1:
+             alert("It's Monday")
+                break;
+        case 2:
+            alert("It's Tuesday")
+            break;
+        default:
+            alert('Its other day')
+            
+    }
+}
+name()
+
+
+
+
+
+
+
+
+
+
+// for (let index = 0; index < elements.length; index++) {
+//     const element = elements[index];
+//     console.log(element.innerHTML);
+// }
