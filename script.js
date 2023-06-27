662ffeb9a9256f216e65f6ce342e8469cda679a3
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-27 15:58:35 +0500

    complete porject

--- script.js
+++ script.js
@@ -2,49 +2,54 @@
 // elements[2].className += " red"
 
 
-// function name() {
+function name() {
     
 
-//     var now = new Date();
-//     var today = now.getDay();
+    var now = new Date();
+    var today = now.getDay();
 
-//     switch (today) {
-//         case 0:
-//             alert("It's sunday")
-//             break;
-//         case 1:
-//              alert("It's Monday")
-//                 break;
-//         case 2:
-//             alert("It's Tuesday")
-//             break;
-//         default:
-//             alert('Its other day')
+    switch (today) {
+        case 0:
+            alert("It's sunday")
+            break;
+        case 1:
+             alert("It's Monday")
+                break;
+        case 2:
+            alert("It's Tuesday")
+            break;
+        default:
+            alert('Its other day')
             
-//     }
+    }
+}
+name()
+
+
+
+// var user = {
+//     firstName : "Ghullam",
+//     lastName : "Mustafa",
+//     city : "Faisalabad",
+//     age : 19 ,
 // }
-// name()
+// console.log(user.firstName);
 
+// var paragraph = document.getElementsByTagName("p");
 
+//             console.log(paragraph[0].innerHTML);
 
-var user = {
-    firstName : "Ghullam",
-    lastName : "Mustafa",
-    city : "Faisalabad",
-    age : 19 ,
-}
-console.log(user.firstName);
+var newParagraph = document.createElement('p')
 
-for (let index = 0; index < user.length; index++) {
-    const element = user[index];
-    console.log(element);
-}
+console.log(newParagraph);
+var newText = document.createTextNode("Hellow!")
+newParagraph.appendChild(newText)
+console.log(newText);
+document.getElementById('abc').appendChild(newParagraph)
 
-
-
-
-
-
+newParagraph.setAttribute("class","red")
+newParagraph.className += " red"
+document.getElementById('abc').appendChild(newParagraph)
 
 
 // for (let index = 0; index < elements.length; index++) {
