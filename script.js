bc2a9bccb8d52aaec65213c5fee01ab582aa5f96
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-27 14:58:15 +0500

    first project

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 // var elements = document.getElementsByTagName("p")
 // elements[2].className += " red"
-function name() {
+
+
+// function name() {
     
 
-    var now = new Date();
-    var today = now.getDay();
+//     var now = new Date();
+//     var today = now.getDay();
 
-    switch (today) {
-        case 0:
-            alert("It's sunday")
-            break;
-        case 1:
-             alert("It's Monday")
-                break;
-        case 2:
-            alert("It's Tuesday")
-            break;
-        default:
-            alert('Its other day')
+//     switch (today) {
+//         case 0:
+//             alert("It's sunday")
+//             break;
+//         case 1:
+//              alert("It's Monday")
+//                 break;
+//         case 2:
+//             alert("It's Tuesday")
+//             break;
+//         default:
+//             alert('Its other day')
             
-    }
-}
-name()
-
-
-
-// var user = {
-//     firstName : "Ghullam",
-//     lastName : "Mustafa",
-//     city : "Faisalabad",
-//     age : 19 ,
+//     }
 // }
-// console.log(user.firstName);
+// name()
+
+
+
+var user = {
+    firstName : "Ghullam",
+    lastName : "Mustafa",
+    city : "Faisalabad",
+    age : 19 ,
+}
+console.log(user.firstName);
+
+for (let index = 0; index < user.length; index++) {
+    const element = user[index];
+    console.log(element);
+}
 
 
 
